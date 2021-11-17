bbea15650de6d6e865756635f847d151eb7e8755
David <david4jsus@gmail.com>
2021-11-16 17:15:01 -0800

    Feat: Created adder functions for some of the data that the app will track
    "Adder" as in "add some data to our database"

--- src/data_manager.js
+++ src/data_manager.js
@@ -2,15 +2,116 @@
    Singleton object that represents the manager that takes care of keeping track of the data this app uses, as well as interacting with it to keep it updated and fetch specific data from it
 */
 
+// Dependencies
+const { FileObj } = require("./file_obj");
+const { FolderObj } = require("./folder_obj");
+
 // Constructor
 const DataManager = function()
 {
-   this.trackedFileData = [];
-   this.trackedFolderData = [];
-   this.tagData = [];
-   this.groupData = [];
+   this.trackedFileData    = [];
+   this.trackedFolderData  = [];
+   this.trackedTagData     = [];
+   this.trackedGroupData   = [];
    this.trackedLibraryData = [];
+
+   this.currentFileID    = 0;
+   this.currentFolderID  = 0;
+   this.currentTagID     = 0;
+   this.currentGroupID   = 0;
+   this.currentLibraryID = 0;
 
    // Load saved data from saved files
    // LoadSavedData();
 }
+
+// Get the next ID to be assigned
+DataManager.prototype.getNextID(type)
+{
+   switch (type)
+   {
+      default:
+         console.error("ERROR: Type of object to assign ID to is invalid!");
+         break;
+      
+      case "file":
+         return currentFileID++;
+         break;
+      
+      case "folder":
+         return currentFileID++;
+         break;
+      
+      case "tag":
+         return currentTagID++;
+         break;
+      
+      case "group":
+         return currentGroupID++;
+         break;
+      
+      case "library":
+         return currentLibraryID++;
+         break;
+   }
+}
+
+// Add a file to the list
+DataManager.prototype.addFile = function(file)
+{
+   this.trackedFileData.push(file);
+}
+
+// Add file data to the list
+DataManager.protytype.addFileData = function(filename, tags, group, library)
+{
+   this.trackedFileData.push(new FileObj(getNextID("file"), filename, tags, group, library));
+}
+
+// Add a folder to the list
+DataManager.prototype.addFolder = function(folder)
+{
+   this.trackedFolderData.push(folder);
+}
+
+// Add folder data to the list
+DataManager.protytype.addFolderData = function(path, children)
+{
+   this.trackedfolderData.push(new FolderObj(getNextID("folder"), path, children));
+}
+
+// Add a tag to the list
+DataManager.prototype.addTag = function(tag)
+{
+   this.trackedTagData.push(tag);
+}
+
+// Add tag data to the list
+// DataManager.protytype.addTagData = function(filename, tags, group, library)
+// {
+//    this.trackedFileData.push(new FileObj(getNextID("file"), filename, tags, group, library));
+// }
+
+// Add a group to the list
+DataManager.prototype.addGroup = function(group)
+{
+   this.trackedGroupData.push(group);
+}
+
+// Add group data to the list
+// DataManager.protytype.addGroupData = function(filename, tags, group, library)
+// {
+//    this.trackedFileData.push(new FileObj(getNextID("file"), filename, tags, group, library));
+// }
+
+// Add a library to the list
+DataManager.prototype.addLibrary = function(library)
+{
+   this.trackedLibraryData.push(library);
+}
+
+// Add library data to the list
+// DataManager.protytype.addLibraryData = function(filename, tags, group, library)
+// {
+//    this.trackedFileData.push(new FileObj(getNextID("file"), filename, tags, group, library));
+// }
